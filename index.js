88c538a846ee28cdad03bc09f03933b528c9a394
Han Wang <hyw2@cornell.edu>
2021-05-09 17:51:12 -0400

    Add npm start command

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cors());
 // 1. Scrape data into relevant JSONL file that can be uploaded
 // 2. Connect GPT-3 with hyw2 credits and search with ada or babbage
 // 3. Build simple frontend
-
 // 4. Deploy to website
 
 app.post('/api/search', async (req, res) => {
